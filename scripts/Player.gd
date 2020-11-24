bcb504b105bbb0cdf1bdd408b1d1cff594e66d4c
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-11-24 20:03:04 +0530

    Add shooting action

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 
 var movespeed = 500
+var bullet_speed= 2000
+var bullet = preload("res://objects/bullet.tscn")
 
 func _ready():
 	pass # Replace with function body.
@@ -16,8 +18,19 @@ func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		motion.x -=1
 	if Input.is_action_pressed("left"):
-		motion.x +=1
+		motion.x +=1 
 		
 	motion = motion.normalized()
 	motion = move_and_slide(motion*movespeed)
-		
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("LMB"):
+		fire()
+
+func fire():
+	var bullet_instance = bullet.instance()
+	bullet_instance.position=get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_instance)
+	
